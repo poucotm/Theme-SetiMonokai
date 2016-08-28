73d40e61d787e6c5f3f1a0269e72308b68d8e0f0
Kris Jeon <poucotm@gmail.com>
2016-08-28 13:25:58 +0900

    ADD : example

---
+++ example/app/src/android.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.test;
+
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This demonstrates the use of action bar tabs and how they interact
+ * with other action bar features.
+ */
+public class Test extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+
+    public void onAddTab(View v) {
+        final ActionBar bar = getActionBar();
+        final int tabCount = bar.getTabCount();
+        final String text = "Tab " + tabCount;
+        bar.addTab(bar.newTab()
+                .setText(text)
+
+    public void onRemoveTab(View v) {
+        final ActionBar bar = getActionBar();
+        bar.removeTabAt(bar.getTabCount() - 1);
+    }
+
+    public void onToggleTabs(View v) {
+        final ActionBar bar = getActionBar();
+
+        }
+    }
+
+    public void onRemoveAllTabs(View v) {
+        getActionBar().removeAllTabs();
+    }
+
+    /**
